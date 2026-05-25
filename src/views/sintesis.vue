1847964a47e19a0993a65badcb8aa825c1aa7729
Henry Alvarez Astudillo <halvarezast@gmail.com>
2026-05-25 09:41:12 -0500

    Update Sintesis

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -12,7 +12,7 @@
     .row.justify-content-center
       .col-lg-10.mb-5.bgs.p-4.brad
         figure
-          img(src="@/assets/curso/sintesis.svg", alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu tincidunt erat. Fusce cursus mauris in nulla egestas vulputate. Phasellus viverra accumsan libero, sed luctus diam maximus euismod. Mauris sem risus, tincidunt vel libero in, lobortis commodo ante. Sed pulvinar luctus orci in rhoncus. Nulla ut dapibus risus, ac tempor est. Donec nulla augue, vehicula quis malesuada vitae, fermentum non nisi. Donec tincidunt nibh a magna bibendum rutrum. ")
+          img(src="@/assets/curso/sintesis.svg", alt="Síntesis del componente formativo sobre Microsoft PowerPoint que organiza contenidos como conceptos de presentaciones, importancia y usos en contextos académicos y laborales, tipos de presentaciones, elementos de la interfaz, flujo de trabajo digital, diseño de diapositivas, uso de plantillas y temas, formato de contenido e integración de recursos como imágenes, gráficos, animaciones, transiciones y presentación final.")
       .col-auto
         a.anexo.mb-5(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
           .anexo__icono
